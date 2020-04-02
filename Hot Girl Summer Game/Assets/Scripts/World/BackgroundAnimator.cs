0f284fc1f46848c51928c2e26ba20161fd80e7d3
MHOSS <hhoossppee@gmail.com>
2020-04-01 22:12:49 -0400

    Background and Portraits

---
+++ Hot Girl Summer Game/Assets/Scripts/World/BackgroundAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnimator : MonoBehaviour
+{
+
+    public Sprite[] frames;
+    public float speed;
+
+    private int currentFrame;
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= speed)
+        {
+            timer -= speed;
+            currentFrame = (currentFrame + 1) % frames.Length;
+            gameObject.GetComponent<SpriteRenderer>().sprite = frames[currentFrame];
+
+        }
+
+
+
+    }
+
+}
