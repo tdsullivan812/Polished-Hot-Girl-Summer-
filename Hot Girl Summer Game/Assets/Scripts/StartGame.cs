59119f697088087e8a126c1a44120033d5936691
scuddamn <aescudder@gmail.com>
2020-04-02 00:11:58 -0400

    added bare bones start menu mechanics and made card game UI more readable

---
+++ Hot Girl Summer Game/Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void clickToPlay()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
