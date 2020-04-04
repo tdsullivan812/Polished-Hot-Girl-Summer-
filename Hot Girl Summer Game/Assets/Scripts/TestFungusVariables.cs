d1759e9b4ba7b9c44ec1eb9f4dc8ff1940d885bb
Thomas Sullivan <tdsullivan812@gmail.com>
2020-04-04 18:06:29 -0400

    Made a simple script to set Fungus variables to the current bubbly, chll, or hype vibes

---
+++ Hot Girl Summer Game/Assets/Scripts/TestFungusVariables.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFungusVariables : MonoBehaviour
+{
+    public Fungus.Flowchart flowchart;
+
+    public Fungus.VariableReference hype;
+    public Fungus.VariableReference bubbly;
+    public Fungus.VariableReference chill;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hype.variable = flowchart.GetVariable("Hype");
+        bubbly.variable = flowchart.GetVariable("Bubbly");
+        chill.variable = flowchart.GetVariable("Chill");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hype.Set<float>((float)GameController.partyDeck.victoryPoints.hypePoints);
+        bubbly.Set<float>((float)GameController.partyDeck.victoryPoints.bubblyPoints);
+        chill.Set<float>((float)GameController.partyDeck.victoryPoints.calmPoints);
+    }
+}
