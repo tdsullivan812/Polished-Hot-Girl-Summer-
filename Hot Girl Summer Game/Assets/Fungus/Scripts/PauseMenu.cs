21f54c7c255ab95b6feea9c41a7718ff765a80c0
scuddamn <aescudder@gmail.com>
2020-04-16 13:47:22 -0400

    added colliders + rigidbodies to all npcs and made them all clickable. connected fungus blocks to respective npcs. added pause menu functionality. changed cardbacks to new version

---
+++ Hot Girl Summer Game/Assets/Fungus/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (gamePaused == false)
+            {
+                gamePaused = true;
+                pauseMenu.SetActive(true);
+
+            }
+            else
+            {
+                gamePaused = false;
+                pauseMenu.SetActive(false);
+            }
+
+        }
+
+    }
+}
