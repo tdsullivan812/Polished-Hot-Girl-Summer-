354c48b7e1f23257d117353a96ff613b35037c65
Thomas Sullivan <tdsullivan812@gmail.com>
2020-04-20 01:03:10 -0400

    Created scripts for the Leon and Kelly card encounters. Their detailed effects are below:
    Leon: Every turn, he adds one Bubbly card to the top of your deck. To satisfy him, you have to play three Dance cards.
    
    Meg: Meg will trigger the last Hype card played this turn a second time. To satisfy her, you need to have at least 16 Hype points.

---
+++ Hot Girl Summer Game/Assets/Scripts/Card Game/NPCs/Leon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leon : NPC
+{
+    private int danceCardsPlayed;
+    public Leon()
+    {
+        npcName = "Leon";
+        npcSprite = null;
+        victoryCondition = RequiredCards;
+        successMessage = "Leon Solved";
+        failureMessage = "Leon Unsolved";
+        turnsExpired = 0;
+        danceCardsPlayed = 0;
+        Services.eventManager.Register<ActionCardPlayed>(CheckWhatCardWasPlayed);
+    }
+
+    public bool RequiredCards()
+    {
+        if (danceCardsPlayed >= 3)
+        {
+            Services.eventManager.Unregister<ActionCardPlayed>(CheckWhatCardWasPlayed);
+            return true;
+        }
+        else return false;
+    }
+
+    public void CheckWhatCardWasPlayed(HotGirlEvent hotGirlEvent)
+    {
+        ActionCardPlayed actionTaken = (ActionCardPlayed)hotGirlEvent;
+        if (actionTaken.cardPlayed.GetType() == typeof(Dance))
+        {
+            danceCardsPlayed++;
+        }
+    }
+    public override void Effect()
+    {
+        Card leonEffectCard = new Bubbly();
+        Encounter.playerDeck.AddToDeck(leonEffectCard, 0);
+    }
+
+
+}
---
+++ Hot Girl Summer Game/Assets/Scripts/Card Game/NPCs/Meg.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meg : NPC
+{
+    List<Card> hypeCardsPlayed;
+
+    public Meg()
+    {
+        npcName = "Meg";
+        npcSprite = null;
+        victoryCondition = RequiredHypePoints;
+        successMessage = "Meg Solved";
+        failureMessage = "Meg Unsolved";
+        turnsExpired = 0;
+        hypeCardsPlayed = new List<Card>();
+        Services.eventManager.Register<ActionCardPlayed>(CheckWhatCardWasPlayed);
+    }
+    public override void Effect()
+    {
+        if (hypeCardsPlayed.Count > 0)
+        {
+            hypeCardsPlayed[hypeCardsPlayed.Count - 1].Effect();
+            hypeCardsPlayed.Clear();
+        }
+        
+    }
+
+    public bool RequiredHypePoints()
+    {
+        if (GameController.partyDeck.victoryPoints.hypePoints >= 16)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    private void CheckWhatCardWasPlayed(HotGirlEvent hotGirlEvent)
+    {
+        ActionCardPlayed action = (ActionCardPlayed)hotGirlEvent;
+        if(action.cardPlayed.displayedInfo.type == Card.Vibes.Hype)
+        {
+            hypeCardsPlayed.Add(action.cardPlayed);
+        }
+
+        
+    }
+
+
+
+
+}
